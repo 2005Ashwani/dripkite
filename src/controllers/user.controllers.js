fa0e978752765776355e9ec08189ddd7189cca6d
2005Ashwani <2005ashwani01@gmail.com>
2025-09-14 13:20:26 +0530

    Initial commit for Customer-SignUp-Page-Done

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,10 +1,13 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import signUp from "../model/signup.model.js";
+import customerSignUp from "../model/signUpCustomer.js"
 import apiError from "../utils/apiError.js";
 import apiResponse from "../utils/apiResponse.js"
 import jwt from "jsonwebtoken"
 import mongoose from "mongoose";
 import uploadOnCloudinary from "../utils/cloudinary.js";
+
+
 
 
 const generateRefreshTokenAndAccessToken = async function (userId) {
@@ -26,44 +29,7 @@ const generateRefreshTokenAndAccessToken = async function (userId) {
 
 }
 
-const registerUser = asyncHandler(async function (req, res, next) {
 
-    const { userName, emailId, phoneNo, password, confirmPassword } = req.body;
-
-
-
-    //validation 
-    if ([userName, emailId, phoneNo, password, confirmPassword].some((elem) => elem === "")) {
-        throw new apiError(400, "All fields are required!")
-    }
-
-    const existUser = await signUp.findOne({
-        $or: [{ userName }, { emailId }, { phoneNo }]
-    })
-
-    if (existUser) { throw new apiError(400, "User already exists!") }
-
-    const user = await signUp.create({
-        userName,
-        emailId,
-        phoneNo,
-        password,
-        confirmPassword
-    })
-
-    const createdUser = await signUp.findById(user._id).select("-password -confirmPassword")
-
-    if (!createdUser) {
-        throw new apiError(400, "Something went wrong while registering User")
-    }
-
-    return res.status(201).json(
-        new apiResponse(200, createdUser, "User registered Successfully!")
-    )
-    //check any of the userName, emailId, phoneNo already exits or not in DB
-
-
-})
 
 
 const loginUser = asyncHandler(async function (req, res, next) {
@@ -106,7 +72,7 @@ const loginUser = asyncHandler(async function (req, res, next) {
 const signoutUser = asyncHandler(async function (req, res, next) {
 
 
-    
+
     await signUp.findByIdAndUpdate(req.user._id,
         {
             $unset: {
@@ -114,19 +80,99 @@ const signoutUser = asyncHandler(async function (req, res, next) {
             }
         },
         {
-            new:true
+            new: true
         }
     )
-    
+
     const options = {
-        httpOnly:true,
-        secure:true
+        httpOnly: true,
+        secure: true
     }
 
     return res
-    .status(200)
-    .clearCookie("accessToken",options)
-    .clearCookie("refreshToken",options)
-    .json(new apiResponse(200,{},"User logged Out"))
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new apiResponse(200, {}, "User logged Out"))
 })
-export { registerUser, loginUser, signoutUser }
+
+
+const registerUser = asyncHandler(async function (req, res, next) {
+
+    const { userName, emailId, phoneNo, password, confirmPassword } = req.body;
+
+
+
+    //validation 
+    if ([userName, emailId, phoneNo, password, confirmPassword].some((elem) => elem === "")) {
+        throw new apiError(400, "All fields are required!")
+    }
+
+    const existUser = await signUp.findOne({
+        $or: [{ userName }, { emailId }, { phoneNo }]
+    })
+
+    if (existUser) { throw new apiError(400, "User already exists!") }
+
+    const user = await signUp.create({
+        userName,
+        emailId,
+        phoneNo,
+        password,
+        confirmPassword
+    })
+
+    const createdUser = await signUp.findById(user._id).select("-password -confirmPassword")
+
+    if (!createdUser) {
+        throw new apiError(400, "Something went wrong while registering User")
+    }
+
+    return res.status(201).json(
+        new apiResponse(200, createdUser, "User registered Successfully!")
+    )
+    //check any of the userName, emailId, phoneNo already exits or not in DB
+
+
+});
+
+
+// Register Customer
+const registerCustomers = async (req, res) => {
+    const { dob, gender } = req.body;
+    try {
+
+
+        // Checking the fields
+        if (!dob) {
+            return res.status(400).json(new apiError(400, "Please enter the DOB"));
+        }
+        if (!["male", "female", "other"].includes(gender)) {
+            return res
+                .status(400)
+                .json(new apiError(400, "Please enter the Gender properly"));
+        }
+
+        // data that require in customer sign up
+        const { userName, emailId, phoneNo } = req.user;
+        console.log(userName, emailId, phoneNo)
+
+
+
+        // Adding Data to DB
+        await customerSignUp.create(req.body);
+
+        // Send the response to the user
+        return res
+            .status(201)
+            .json("Customer registered successfully!");
+
+
+    } catch (error) {
+        return res
+            .status(500)
+            .json(new apiError(500, "Something went wrong while registering customer", error.message));
+    }
+}
+
+export { registerUser, loginUser, signoutUser, registerCustomers }
